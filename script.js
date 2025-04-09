30027d2205d3bb443a8b9adcaf614a4fd3999fe6
nishah8 <141764574+nishah8@users.noreply.github.com>
2025-04-09 09:48:00 +0100

    Update script.js
    updated to new solution -n

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 
+// Add a click event listener to toggle the menu visibility
 hamburger.addEventListener('click', function () {
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('show'); 
 });
